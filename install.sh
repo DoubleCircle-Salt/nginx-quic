5a41acd2ef5a9a39c3988cb2ca6e925f7f524ab8
root <root@US.LAX5.838.E.141.109>
2022-02-17 16:25:13 +0800

    commit

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 ./auto/configure \
  --prefix=/usr/local/nginx --with-http_ssl_module --with-http_v2_module --with-http_v3_module --with-cc-opt="-I../boringssl/include" --with-ld-opt="-L../boringssl/build/ssl -L../boringssl/build/crypto" \
-	--with-stream --with-stream_quic_module
+	--with-debug --with-stream --with-stream_quic_module
